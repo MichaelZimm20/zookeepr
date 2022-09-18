b99a40875fcc105e1e779db85909e97c3339adf7
DJMIKE20 <m.zimm20@gmail.com>
2022-09-18 17:52:59 -0400

    updated port to accept 3001

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 //requests for animals
@@ -68,6 +69,6 @@ app.get('/api/animals', (req, res) =>{
 
 
 //express to listen
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
